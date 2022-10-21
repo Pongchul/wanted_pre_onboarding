88d5e69ce8d47f2bd47770f5758148cabf3e2df3
Pongchul <inchul7511@gmail.com>
2022-10-22 00:15:10 +0900

    Add Application Viewset !

--- api/views.py
+++ api/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render, get_object_or_404
-from rest_framework import viewsets, status
+from django.shortcuts import  get_object_or_404
+from rest_framework import viewsets, status, filters
 from rest_framework.response import Response
 
 from api import serializers
-from api.models import User, Post, Company
+from api.models import User, Post, Company, Apply
 
 
 # Create your views here.
@@ -56,4 +56,47 @@ class PostViewSet(viewsets.ViewSet):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
+class ApplyViewSet(viewsets.ViewSet):
+    queryset = Apply.objects.all()
+    serializers_class = serializers.ApplySerializer
 
+    def create(self, request, *args, **kwargs):
+        serializer = serializers.ApplySerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def list(self, request, *args, **kwargs):
+        queryset = Apply.objects.all()
+        serializer = serializers.ApplySerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = Apply.objects.all()
+        apply = get_object_or_404(queryset, pk=pk)
+        serializer = serializers.ApplySerializer(apply)
+        return Response(serializer.data)
+
+    def update(self, request, pk=None):
+        queryset = Apply.objects.all()
+        apply = get_object_or_404(queryset, pk=pk)
+
+        serializer = serializers.ApplySerializer(apply, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def destroy(self, request, pk=None):
+        queryset = Apply.objects.all()
+        apply = get_object_or_404(queryset, pk=pk)
+        apply.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class SearchKeyWordViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = serializers.PostSerializer
+
+    filter_backends = [filters.SearchFilter]  # settings.py에 DEFAULT_FILTER_BACKENDS 추가
+    search_fields = ['company_name','position','skill']
