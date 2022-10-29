b614081ec02f89bf0ea0f0e8729066465b7df5aa
Pongchul <inchul7511@gmail.com>
2022-10-30 01:42:22 +0900

    Add company url !

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,7 @@ from rest_framework import routers
 
 router = routers.DefaultRouter()
 
+router.register(r'company', views.CompanyViewSet, basename='company')
 router.register(r'user', views.UserViewSet, basename='user')
 router.register(r'post', views.PostViewSet, basename='post')
 router.register(r'company', views.PostViewSet, basename='company')
