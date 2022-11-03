03a11921ffca0605638924599694e08a409672c3
Pongchul <inchul7511@gmail.com>
2022-11-03 15:52:35 +0900

    url 추가 !

--- api/urls.py
+++ api/urls.py
@@ -9,7 +9,7 @@ router.register(r'company', views.CompanyViewSet, basename='company')
 router.register(r'user', views.UserViewSet, basename='user')
 router.register(r'post', views.PostViewSet, basename='post')
 router.register(r'company', views.PostViewSet, basename='company')
-router.register(r'search', views.)
+router.register(r'search', views.SearchKeyWordViewSet,)
 
 
 urlpatterns = [
